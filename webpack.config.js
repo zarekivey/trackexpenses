8c3868b882bf1599604cd04daa1b01bc34dd86ed
Malachi Ivey <zarekbusiness@gmail.com>
2018-12-07 10:40:49 -0500

    setup prod webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
 const path = require('path'); // Node method to get the path
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = { // This is where we configure all of the details
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new MiniCssExtractPlugin({ filename: 'styles.css' }); 
+
+    console.log('env', env)
+    return { // This is where we configure all of the details
     entry: './src/app.js', // Where webpack should start
     output: {
         path: path.join(__dirname, 'public'), // The ABSOLUTE path to where want to output the webapack file
@@ -8,21 +14,35 @@ module.exports = { // This is where we configure all of the details
     },
     module: {
         rules: [{
-            loader: 'babel-loader', // What module we want to load
+            loader:'babel-loader', // What module we want to load
             test: /\.js$/, // What types of files we want to load it into
             exclude: /node_modules/ // Where we want to exclude it from 
         }, {
             test: /\.s?css$/, // Targeting all of our scss files
-            use: [ // use allows for an array of loaders
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+            use: [
+                MiniCssExtractPlugin.loader,
+                {
+                    loader: 'css-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                },
+                {
+                    loader: 'sass-loader',
+                    options: {
+                        sourceMap: true
+                    }
+                }
             ]
         }] 
     },
-    devtool: 'cheap-module-source-map', // More options can be found on the webpack website. this allows for better debugging
+    plugins: [
+        CSSExtract
+    ],
+    devtool: isProduction ? 'source-map' : 'inline-source-map', // More options can be found on the webpack website. this allows for better debugging
     devServer: {
         contentBase: path.join(__dirname, 'public'), // Where we want to host the live server
         historyApiFallback: true, // This tells the server we'll be handling routing via our client side code
     }
+};
 };
