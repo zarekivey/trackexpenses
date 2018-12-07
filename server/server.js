c484d5f1e7aa8b5517e50b146f94506725411373
Malachi Ivey <zarekbusiness@gmail.com>
2018-12-07 12:00:39 -0500

    setup production server

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express'); //  importing in node
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public') // this is to serve up express to all apps in this directory
+const port = process.env.PORT || 3000 // heroku variable
 
 app.use(express.static(publicPath));
 
@@ -9,6 +10,6 @@ app.get('*', (req, res) => { // * acts as a link to the directory
     res.sendFile(path.join(publicPath, 'index.html')); // this is directing the user to index.html
 }); 
 
-app.listen(3000, () => { //  where the server will run 
+app.listen(port, () => { //  where the server will run 
     console.log('server is up')
 });
