650f7b8463543ae8913c1dbd360261cb9126ccaa
Malachi Ivey <zarekbusiness@gmail.com>
2018-12-14 13:42:13 -0500

    setup test database env

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import * as firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyBNtBURkW7LzVrRw2WwskbdoXwsGxerdUA",
-    authDomain: "track-expenses-31ef6.firebaseapp.com",
-    databaseURL: "https://track-expenses-31ef6.firebaseio.com",
-    projectId: "track-expenses-31ef6",
-    storageBucket: "track-expenses-31ef6.appspot.com",
-    messagingSenderId: "580570047116"
-  };
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  };  
 
 firebase.initializeApp(config);
 
