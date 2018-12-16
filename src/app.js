3d805c2d78bf9347ac54a05521b0acde0ae24994
Malachi Ivey <zarekbusiness@gmail.com>
2018-12-16 14:47:17 -0500

    added loading page, corrected snapshots

--- src/app.js
+++ src/app.js
@@ -10,8 +10,8 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css'
 import { firebase } from './firebase/firebase'
-import './playground/promises'
 import { userInfo } from 'os';
+import LoadingPage from './components/LoadingPage'
 
 const store = configureStore(); // This gives us access to all of the stores methods
 
@@ -28,7 +28,7 @@ const renderApp = () => {
     }
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app')); 
+ReactDOM.render(<LoadingPage />, document.getElementById('app')); 
 
 firebase.auth().onAuthStateChanged((user) => {
     if (user) {
