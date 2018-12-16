c74fe9700d05f02b2e9ec03489a00ebcbcf10ea1
Malachi Ivey <zarekbusiness@gmail.com>
2018-12-16 10:50:56 -0500

    added styling for the header

--- src/components/ExpensesSummary.js
+++ src/components/ExpensesSummary.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import React from 'react'
+import { Link } from 'react-router-dom';
 import numeral from 'numeral'
 import selectExpenses from '../selectors/expenses'
 import selectExpensesTotal from '../selectors/expenses-total'
@@ -8,9 +9,14 @@ export const ExpensesSummary = ({ expenseCount, expensesTotal }) => {
     const expenseWord = expenseCount === 1 ? 'expense' : 'expenses';
     const formattedExpensesTotal = numeral(expensesTotal / 100).format('$0,0.00')
     return (
-    <div>
-        <h1>Viewing {expenseCount} {expenseWord} totalling {formattedExpensesTotal}</h1>
-    </div>
+    <div className="page-header">
+        <div className="content-container">
+            <h1 className="page-header__title">Viewing <span>{expenseCount} {expenseWord}</span> totalling <span>{formattedExpensesTotal}</span></h1>
+            <div classNAme="page-header_actions">
+                <Link className="button" to="/create">Add Expense</Link>
+            </div>
+        </div>
+    </div> 
     );
 };
 
