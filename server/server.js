0b7fa8e13a87d1567a8e10258a2a5101601b841b
Malachi Ivey <zarekbusiness@gmail.com>
2019-02-18 13:57:24 -0500

    cors

--- server/server.js
+++ server/server.js
@@ -3,8 +3,11 @@ const express = require('express'); //  importing in node
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public') // this is to serve up express to all apps in this directory
 const port = process.env.PORT || 3000 // heroku variable
+const cors = require('cors')({origin: true});
+
 
 app.use(express.static(publicPath));
+app.use(cors());
 
 app.get('*', (req, res) => { // * acts as a link to the directory
     res.sendFile(path.join(publicPath, 'index.html')); // this is directing the user to index.html
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,4 +1,5 @@
 import * as firebase from 'firebase';
+const cors = require('cors')({origin: true});
 
 
 const config = {
